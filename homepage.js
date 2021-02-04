367d4551604267f9fa8cb762775b525ea061fe9a
kaiete <kaiete@aragon-press.com>
2021-02-04 08:29:34 +0000

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -20,3 +20,6 @@ document.getElementById("searchterm").hidden = false
 } else {
 document.getElementById("searchterm").hidden = true 
 }
+function hidenosearch() {
+  document.getElementById("searchterm").hidden = true
+}
