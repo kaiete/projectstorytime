d40ac295fe65b409e7216ed174160179a2aafe09
kaiete <kaiete@aragon-press.com>
2021-02-11 15:39:03 +0000

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -17,31 +17,42 @@ var qt = {
   },
   log : function(msg) {
     console.log(msg)
+    return "undefined"
   },
   link : function(site) {
     window.location.href=site
   },
   redir : function(site) {
     window.location.replace(site)
+  },
+  params : function() {
+    let parameters = new URLSearchParams(location.search)
+    let editedparams = decodeURIComponent(parameters)
+    delete parameters
+    let params = editedparams
+    return params
+  },
+  if : function(statementToCheck,toRunIfTruthy,toRunIfFalsy) {
+    if (statementToCheck) {
+      return true
+      eval(toRunIfTruthy)
+    } else if (not statementToCheck) {
+      return false
+      eval(toRunIfFalsy)
+    } else {
+      return "QT-Error: returned neither true nor false"
+      console.log("QueryTools ran into an error, and this site might not work correctly. We recommend contacting the developer and telling them that QueryTools found an invalid qt.if statement.")
+    }
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 function authuser() {
-var user = prompt("username:") 
-var userok = prompt(user,"true/false") 
-if (userok == "true") {
-document.getElementById("signin").disabled = true
-setTimeout('alert("done")',200)
+var user = qt.ask("username:") 
+var userok = prompt("Continue as " + user + "? Type true or false") 
+qt.if(userok, document.getElementById("signin").disabled = true;setTimeout('alert("done")',200), delete user;alert("username removed from client info");document.getElementById("signin").disabled = false)
 }
-if (userok == "false") {
-delete user
-alert("username removed from client info") 
-document.getElementById("signin").disabled = false
-}
-}
-var userok = "false"
-const parameters = new URLSearchParams(window.location.search)
+const parameters = qt.params()
 var searchterm = parameters.toString().replace("s=","") 
 var search
 parameters.replace("s=","") == "" ? search = true : search = false
